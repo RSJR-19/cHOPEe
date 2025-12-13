b1bed50fb75edd253a2d435ed9c67f01fe13aa6a
Seyri <sumbadrenan619@gmail.com>
2025-12-13 21:44:07 +0800

    First prototype

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const STATES = {
     COFFEE_EMPTY : "coffee_empty",
     PREP_COFFEE : "prep_coffee",
     RETURN_COFFEE : 'return_coffee',
+    COFFEE_READY : 'coffee_ready',
     QUOTE_REVEAL : 'quote_reveal'
 }
 let testOnPlayThrough = false; //remove later
+let coffee_state_bool = false
 
 const loading_screen = document.getElementById(STATES.LOADING);
 const main_screen = document.getElementById('main');
@@ -16,11 +18,36 @@ const coffee_state = document.getElementById('coffee_state');
 loading_screen.style.display = 'flex';
 
 function prepareCoffee(){
+    coffee_state.innerHTML = "full";
     setTimeout(()=>{
         prep_coffee.style.display = 'none';
         main_screen.style.display = 'flex';
+        returnCoffee()
+        
     
     }, 2000)
+}
+
+function returnCoffee(){
+    setTimeout(()=>{
+        cup.classList.remove('addCoffee');
+        coffeeReady()
+    }, 50)
+}
+
+function coffeeReady(){
+    setTimeout(()=>{
+        coffee_state_bool = true;
+    }, 100)
+}
+
+function revealQuote(){
+    setTimeout(()=>{
+        coffee_state.innerHTML = "If there's a will \n there's a way"
+        coffee_state.style.textAlign = 'center';
+        coffee_state.style.fontSize = '5px';
+        coffee_state_bool = false;
+    }, 1000)
 }
 
 setTimeout(()=>{
@@ -29,11 +56,18 @@ setTimeout(()=>{
 }, 0) // for testing only
 
 cup.addEventListener('click',()=>{
+    if (coffee_state_bool != true){
     cup.classList.add('addCoffee');
     setTimeout(()=>{
         main_screen.style.display = 'none';
         prep_coffee.style.display = 'flex'
         prepareCoffee();
     },500)
+}   
+    else{
+        coffee_state.innerHTML = "";
+        cup.classList.add('zoom');
+        revealQuote();
+    }
 
 })
