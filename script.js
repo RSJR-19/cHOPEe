34c6b46eac542170f8dd2b12b65aa71794cf45ad
Seyri <sumbadrenan619@gmail.com>
2025-12-15 16:38:41 +0800

    before adding appWrapper

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const loadingScreen = document.getElementById('loading');
+const coffeeEmptyScreen = document.getElementById('coffeeEmpty');
+
+
+const testButton = document.getElementById('test-button');
+const hand = document.getElementById('hand');
+const cup = document.getElementById('cup');
+
 
 
 const STATES = {
     LOADING : loadingScreen,
-    COFFEE_EMPTY : "coffee_empty",
+    COFFEE_EMPTY : coffeeEmptyScreen,
     PREP_COFFEE : "prep_coffee",
     RETURN_COFFEE : 'return_coffee',
     COFFEE_READY : 'coffee_ready',
@@ -13,11 +20,17 @@ const STATES = {
 
 let currentState = STATES.LOADING;
 
-const setState = (state) =>{
-    document.querySelectorAll(".state").forEach(screenState => screenState.style.display = "none")
-    state.style.display = "flex";
+const setState = () =>{
+    console.log(`${currentState} curently displaying`)
+    document.querySelectorAll(".screenstate").forEach(screenState => screenState.style.display = "none")
+    currentState.style.display = "flex";
 }
 
 setState(STATES.LOADING);
 
+setTimeout(()=>{
+    currentState = STATES.COFFEE_EMPTY
+    setState()},1000)
 
+testButton.addEventListener('click', ()=> hand.classList.add('grab'));
+    
