5a5324089bcfe647dff40b226f0ca603269b2e6a
Seyri <sumbadrenan619@gmail.com>
2025-12-19 14:27:12 +0800

    default

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const loadingScreen = document.getElementById('loading');
-const coffeeEmptyScreen = document.getElementById('coffeeEmpty');
-
-
-const testButton = document.getElementById('test-button');
-const hand = document.getElementById('hand');
-const cup = document.getElementById('cup');
-
 
 
 const STATES = {
-    LOADING : loadingScreen,
-    COFFEE_EMPTY : coffeeEmptyScreen,
+    LOADING : 'loading_screen',
+    COFFEE_EMPTY : 'coffee_empty',
     PREP_COFFEE : "prep_coffee",
     RETURN_COFFEE : 'return_coffee',
     COFFEE_READY : 'coffee_ready',
@@ -18,25 +10,5 @@ const STATES = {
 }
 
 
-let currentState = STATES.LOADING;
 
-const setState = () =>{
-    console.log(`${currentState} curently displaying`)
-    document.querySelectorAll(".screenstate").forEach(screenState => screenState.style.display = "none")
-    currentState.style.display = "flex";
-}
-
-setState(STATES.LOADING);
-
-setTimeout(()=>{
-    currentState = STATES.COFFEE_EMPTY
-    setState()},1000)
-
-testButton.addEventListener('click', ()=> {
-    hand.classList.add('grab');
-    setTimeout(()=>{
-         cup.classList.add('move');
-        hand.classList.remove('grab');
-    }, 900);
-});
     
