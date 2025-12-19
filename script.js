1b58e03770ed7deab76dbd4d0d8868a13b38e38e
Seyri <sumbadrenan619@gmail.com>
2025-12-19 17:39:54 +0800

    before hand and cup agaaaaain

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const loadingScreen = document.getElementById('loading');
+const mainScreen = document.getElementById('main');
+const coffeeEmptyScreen = document.getElementById('coffee_empty');
 
+
+mainScreen.style.display = 'none';
 
 const STATES = {
     LOADING : 'loading_screen',
@@ -9,6 +14,35 @@ const STATES = {
     QUOTE_REVEAL : 'quote_reveal'
 }
 
-
-
+const setStatus = (status)=>{
+    document.querySelectorAll('.screenState').forEach(stateScreen => stateScreen.style.display ="none");
+    status.style.display = 'flex';
     
+}
+
+const stateMachine = (currentState)=>{
+    switch(currentState){
+        case STATES.LOADING:
+            setStatus(loadingScreen);
+            break
+        
+        case STATES.COFFEE_EMPTY:
+            setStatus(coffeeEmptyScreen);
+            break
+    }
+}
+
+window.addEventListener('load', ()=>{
+    stateMachine('loading_screen');
+    setTimeout(()=>{
+        mainScreen.style.display = 'flex';
+        stateMachine('coffee_empty')
+    },1000);
+});
+
+
+
+
+
+
+
