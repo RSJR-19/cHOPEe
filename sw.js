7d02c584f8f0261da2c0fc3267e0c909063e5e61
Seyri <sumbadrenan619@gmail.com>
2026-01-02 10:27:01 +0800

    sw fixes

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'cHOPEe-v1';
+const CACHE_NAME = 'cHOPEe-v1.1';
 
 const ASSETS_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/logo_icon.png",
-    "/logo_icon_bnw.png",
-    '/Empty_Cup.svg',
-    '/Filled_cup.svg',
-    '/full-bnw.svg',
-    '/full-colored.svg',
-    '/manifest.json',
-    '/cafe_bgm.mp3',
-    '/click.mp3',
-    '/order.mp3',
-    '/pour.mp3',
-    '/quotes.js',
-    '/short.svg',
-    '/Plate.svg'
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./logo_icon.png",
+    "./logo_icon_bnw.png",
+    './Empty_Cup.svg',
+    './Filled_cup.svg',
+    './full-bnw.svg',
+    './full-colored.svg',
+    './manifest.json',
+    './cafe_bgm.mp3',
+    './click.mp3',
+    './order.mp3',
+    './pour.mp3',
+    './quotes.js',
+    './short.svg',
+    './Plate.svg'
 ]
 
 self.addEventListener('install', event =>{
