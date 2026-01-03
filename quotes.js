e1ab602f2841a4cba44b890bc93a313b09f60bff
Seyri <sumbadrenan619@gmail.com>
2026-01-03 20:08:37 +0800

    added quotes.json

--- quotes.js
+++ quotes.js
@@ -12,7 +12,20 @@ const quotes = {
     7 : "You deserve better — so grow into better",
     8 : "A good life grows from good decisions",
     9 : "Happiness can be found in simplicity.",
-    10 : "Why doubt yourself? You matter more than you think"
+    10 : "Why doubt yourself? You matter more than you think",
+    11 : "Your story is for you to write - make it a good one",
+    12 : 'Small steps are better than no steps at all',
+    13 : 'Consistency takes you further than perfection',
+    14 : 'We all have our own pace — grow in your own time',
+    15 : 'As long as you make a difference, there is progress',
+    16 : 'Patience is believing that someday, things will make sense',
+    17 : 'Good things take time and dedication',
+    18 : 'Waking up is a gift and another chance to grow',
+    19 : 'Believe in yourself first; others will follow',
+    20 : 'Motivation reminds you. Discipline carries you forward',
+    21 : "Building yourself takes time — and that's okay",
+    22 : "Not every victory needs applause",
+    23 : "You've faced challenges before — you can do it again"
  }
 
 const getQuoteToday =(day)=>{
